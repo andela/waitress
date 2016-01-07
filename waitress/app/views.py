3857b6b2b5e3532c6f118d956e9a1b925c49c424
Chidiebere Nnadi <chidiebere.nnadi@gmail.com>
2016-01-07 01:36:14 +0100

    (chore) Fixed minor bugs to get front-end working

--- waitress/app/views.py
+++ waitress/app/views.py
@@ -1,6 +1,6 @@
 import os
 from django.conf import settings
-from django.views.generic import View
+from django.views.generic import View, TemplateView
 from django.core.urlresolvers import reverse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
--- waitress/settings/base.py
+++ waitress/settings/base.py
@@ -35,7 +35,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'app.middleware.SocialBinderMiddleware',
+    # 'app.middleware.SocialBinderMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
