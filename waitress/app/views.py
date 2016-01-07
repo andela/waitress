f98a5014e9b830837a13a4247c0996c422fa8064
Chidiebere Nnadi <chidiebere.nnadi@gmail.com>
2016-01-07 01:08:21 +0100

    (chore) Bootstrapping the front-end of the application

--- waitress/app/views.py
+++ waitress/app/views.py
@@ -4,3 +4,6 @@ from django.views.generic import View
 from django.core.urlresolvers import reverse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
+
+class HomeView(TemplateView):
+    template_name = 'app/home.html'
