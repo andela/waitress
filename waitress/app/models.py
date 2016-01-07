b9c9e429c40ae39051610361aaefa26657cade34
Oluwafemi Sule <oluwafemi.sule@andela.com>
2016-01-07 22:31:12 +0100

    (feature) update docstring; add is_tapped model instance method

--- waitress/app/models.py
+++ waitress/app/models.py
@@ -1,21 +1,31 @@
-import json
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.utils import timezone
+import json
 
 
 class SlackUserManager(models.Manager):
-    '''Manages `SlackUser` model'''
+    """
+    A class that manages `SlackUser` model
+    """
     def __init__(self, *args):
+        """
+        A initialization method that setup methods and properties of this class
+        from its parent
+        """
         super(SlackUserManager, self).__init__()
 
     def create_user(self, *args, **kwargs):
-        '''Create a new user'''
+        """
+        An instance method that creates a new user
+        """
         return SlackUser(**kwargs)
 
 
 class SlackUser(AbstractBaseUser):
-    '''Represents a `SlackUser` account'''
+    """
+    A class that represents a `SlackUser` account
+    """
     id = models.IntegerField(unique=True, primary_key=True)
     slack_id = models.CharField(unique=True, max_length=20)
     firstname = models.CharField(max_length=20,)
@@ -27,19 +37,39 @@ class SlackUser(AbstractBaseUser):
 
     @classmethod
     def create(cls, user_data_dict):
-        '''Creates a new user'''
-        # import pdb; pdb.set_trace()
+        """
+        A class method that creates a new user
+        """
         return cls.objects.create(**user_data_dict)
 
     def __unicode__(self):
+        """
+        An instance method that return a string representation of this model
+        """
         return "{} {} {}".format(str(self.id), self.firstname, self.lastname)
+
+    def is_tapped(self):
+        time_now = timezone.now()
+        date_today = time_now.date()
+        mealservice = self.mealservice_set.filter(date=date_today)
+        if not mealservice.count():
+            return False
+        if 0 < time_now.hour < 12:
+            return mealservice[0].breakfast
+        return mealservice[0].lunch
 
 
 def untapped_default():
+    """
+    A method that sets the default field value
+    """
     return json.dumps([])
 
 
 class JSONField(models.TextField):
+    """
+    A custom JSON field
+    """
     description = "A JSON field representing a untapped event"
 
     def __init__(self, value, *args, **kwargs):
@@ -47,6 +77,9 @@ class JSONField(models.TextField):
         super(JSONField, self).__init__(args)
 
     def deconstruct(self):
+        """
+        A method which deconstructs a JSON value for db entry
+        """
         name, path, args, kwargs = super(JSONField, self).deconstruct()
         # Only include kwarg if it's not the default
         if self.value != "":
@@ -56,19 +89,27 @@ class JSONField(models.TextField):
         return name, path, args, kwargs
 
     def from_db_value(self, value, expression, connection, context):
+        """
+        A method which recontructs a JSON value from its db entry
+        """
         if value == '':
             return self.value
         value = value.replace("'", '"')
         return json.loads(str(value))
 
     def to_python(self, value):
+        """
+        A method which recontructs a JSON value/dict from its db entry
+        """
         if value == '':
             return self.value
         return json.loads(value)
 
 
-class Service(models.Model):
-    '''Represents a service record for a day'''
+class MealService(models.Model):
+    """
+    A model that represents a meal service record for a day
+    """
     breakfast = models.BooleanField(default=False)
     lunch = models.BooleanField(default=False)
     user = models.ForeignKey(SlackUser)
@@ -78,10 +119,15 @@ class Service(models.Model):
 
     @classmethod
     def create(cls, service_dict):
-        '''Creates a served data record'''
+        """
+        A method that creates a meal served data record
+        """
         return cls.objects.create(**service_dict)
 
     def __unicode__(self):
+        """
+        A method that returns a printable representation of the model
+        """
         has_untapped = len(self.untapped) != 0
         return "{user} {has_breakfast} {has_lunch} {has_untapped} {date}"\
             .format(
@@ -91,15 +137,22 @@ class Service(models.Model):
 
 
 class Passphrase(models.Model):
-    '''Represents a passphrase'''
+    """
+    A model that represents a passphrase
+    """
     word = models.CharField(max_length=100, unique=True)
 
 
 class MealSession(models.Model):
-    '''Represents a meal session'''
+    """
+    A model that represents a meal session
+    """
     status = models.BooleanField(default=False)
     date = models.DateField()
 
-    @property
-    def in_progress(self):
-        return MealSession.objects.get(date=timezone.now().date())
+    @classmethod
+    def in_progress(cls):
+        """
+        A property that returns meal session for the current day
+        """
+        return cls.objects.filter(date=timezone.now().date(), status=True)
