5f860eb9dfb42999f10e6fe12443cbcabcc3cbd9
andela-ashuaib <abiodun.shuaib@andela.com>
2016-02-09 10:45:36 +0100

    (fix) fix error message on login screen

--- ionic-client/waitress/www/js/directives/showDialogDirective.js
+++ ionic-client/waitress/www/js/directives/showDialogDirective.js
@@ -43,8 +43,8 @@ function showDialogDirective($ionicPopup, MealSession, $state, $log) {
                   .then(function() {
                     dialog.close();
                     $state.go('dashboard.tap');
-                  }).catch(function() {
-                    scope.errorMessage = 'Error connecting to server, Please try again later';
+                  }).catch(function(res) {
+                    scope.errorMessage = res.statusText;
                   });
                 } else {
                   scope.errorMessage = 'password can\'t be blank';
