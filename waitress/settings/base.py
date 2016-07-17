91bf3c1198204db7467d04f3eac2639dfe352906
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-07-17 23:46:10 +0100

    remove whitenoise

--- waitress/settings/base.py
+++ waitress/settings/base.py
@@ -99,8 +99,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "staticfiles"),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
