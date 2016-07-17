b073a3f31905c39553a6210a39c41891b610a969
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-07-17 16:21:03 +0100

    update wsgi for deployment in openshift

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='Waitress',
+    version='1.0',
+    description='Waitress',
+    author='Waitress Andela',
+    author_email='waitress-andela@mailserver.com',
+    url='http://www.python.org/sigs/distutils-sig/',
+    install_requires=['Django==1.8'],
+)
--- waitress/wsgi.py
+++ waitress/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for photo_editor project.
+WSGI config for waitress project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,6 +8,14 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+
+if os.getenv('OPENSHIFT_PYTHON_DIR'):
+    virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+    virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+    try:
+        execfile(virtualenv, dict(__file__=virtualenv))
+    except IOError:
+        pass
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
