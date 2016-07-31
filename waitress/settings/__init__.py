b80726a5a36480b4146fc4df89ad96a738aa2091
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-08-01 00:38:17 +0100

    [fix] Use production settings in Heroku

--- waitress/settings/__init__.py
+++ waitress/settings/__init__.py
@@ -4,5 +4,7 @@ if os.getenv('OPENSHIFT_REPO_DIR'):
     from .staging import *
 elif os.getenv('TRAVIS_CI'):
     from .testing import *
+elif os.getenv('HEROKU'):
+    from .production import *
 else:
     from .development import *
