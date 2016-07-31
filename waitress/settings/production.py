147a37806a2f170bcf8567b7af3a4c48d56859ef
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-08-01 00:51:56 +0100

    [fix] Update production settings

--- waitress/settings/production.py
+++ waitress/settings/production.py
@@ -2,10 +2,14 @@
 from .base import *
 import dj_database_url
 
-DEBUG = False
+DEBUG = True
 
 DATABASES = {
     'default': dj_database_url.config()
 }
 
 DATABASES['default']['ENGINE'] = 'django_postgrespool'
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
