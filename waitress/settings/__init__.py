5bd655e69444993d345e2e365fa411898a0082c5
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-07-31 17:39:43 +0100

    Restructure app entry point for deployment to Openshift shift

---
+++ waitress/settings/__init__.py
@@ -0,0 +1,8 @@
+import os
+
+if os.getenv('OPENSHIFT_REPO_DIR'):
+    from staging import *
+elif os.getenv('TRAVIS_CI'):
+    from testing import *
+else:
+    from development import *
---
+++ wsgi.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+import os, sys
+
+
+if os.getenv('OPENSHIFT_REPO_DIR'):
+    sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR']))
+    virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+    os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python2.7/site-packages')
+    virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+    try:
+        execfile(virtualenv, dict(__file__=virtualenv))
+    except IOError:
+        pass
+
+#
+
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'waitress.settings'
+
+# IMPORTANT: Put any additional includes below this line.  If placed above this
+# line, it's possible required libraries won't be in your searchable path
+#
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
