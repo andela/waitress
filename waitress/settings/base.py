5c77d152524ba62348ffae01e1abd867daab7b05
Olufunmilade Oshodi <olufunmilade.oshodi@andela.com>
2016-08-20 17:52:47 +0100

    Tweaked production settings for Heroku

--- waitress/settings/base.py
+++ waitress/settings/base.py
@@ -15,9 +15,6 @@ PROJECT_ROOT = os.path.dirname(BASE_DIR)
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
--- waitress/settings/production.py
+++ waitress/settings/production.py
@@ -2,7 +2,9 @@
 from .base import *
 import dj_database_url
 
-DEBUG = True
+PRODUCTION_DOMAIN = 'waitressandela.herokuapp.com'
+
+DEBUG = False
 
 DATABASES = {
     'default': dj_database_url.config()
@@ -12,4 +14,21 @@ DATABASES['default']['ENGINE'] = 'django_postgrespool'
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [PRODUCTION_DOMAIN]
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
