de05dab77f672eec591a309edd6bb5f8aedb0530
Bolaji Olajide <cooproton@gmail.com>
2019-08-05 01:11:18 +0100

    create auth page for dashboard

--- waitress/settings/production.py
+++ waitress/settings/production.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 import dj_database_url
+from dotenv import load_dotenv, find_dotenv
 
 from .base import *
 
-PRODUCTION_DOMAIN = "waitressandela.herokuapp.com"
+load_dotenv(find_dotenv())
+PRODUCTION_DOMAIN = os.getenv("PROD_DOMAIN")
 
 DATABASES = {"default": dj_database_url.config(conn_max_age=600, ssl_require=True)}
 
